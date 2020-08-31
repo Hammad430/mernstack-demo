56e8ffade18a577bb17dcc511aabe54dc59e4b75
Hammad430 <hammadtahir.1990@hotmail.com>
2020-09-01 01:35:03 +1000

    remove build from gitignore

---
+++ client/build/precache-manifest.c582e567d945b271b86fd25bca7fbbaf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92efb358d7b1c441655c3ddad8d2aaab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d83cea3dd7d923dbc614",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9b9f23dc319332acf53d",
+    "url": "/static/css/main.21ff3d8d.chunk.css"
+  },
+  {
+    "revision": "d83cea3dd7d923dbc614",
+    "url": "/static/js/2.f7be20c6.chunk.js"
+  },
+  {
+    "revision": "5322f0fb94c7f1b79de2c1ce88dfa6fb",
+    "url": "/static/js/2.f7be20c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b9f23dc319332acf53d",
+    "url": "/static/js/main.d345cb94.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "92c89e9c5dfe8d8fca48349fd1e7f818",
+    "url": "/static/media/img1.92c89e9c.jpg"
+  },
+  {
+    "revision": "07632929bc88e244e018ea282bf0ba4f",
+    "url": "/static/media/img2.07632929.jpg"
+  },
+  {
+    "revision": "60d8d791f304c794612a99d164b46ae6",
+    "url": "/static/media/img3.60d8d791.jpg"
+  }
+]);
